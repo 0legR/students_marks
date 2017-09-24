4e39cc7941502859198be6646b00b7d2cfd33ebb
олег ростов <terabyte.or@gmail.com>
2017-09-24 14:11:38 +0300

    implemeted signup functionality

---
+++ app/Http/Controllers/Users/UserController.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Http\Controllers\Users;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function store(Request $request)
+    {
+
+        $rules = [
+    		'username' => 'required|unique:users,username|max:200',
+    		'email' => 'required|email|unique:users,email',
+    		'password' => 'required|min:3',
+    		'passwordConfirmation' => 'required|min:3|same:password',
+    		'timezone' => 'required'
+    	];
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+        	return response()->json($validator->errors(), 422);
+        }
+
+        $user = new User();
+        $userAttributes = $request->only([
+        	'username',
+        	'email',
+        	'timezone'
+        ]);
+        $user->fill($userAttributes);
+        $user->usertype = User::TYPE_ADMIN;
+        $user->fill([
+        	'password' => Hash::make($request->password)
+        ]);
+
+        $user->save();
+
+    	return response()->json(['success' => true]);
+    }
+
+    public function sendUser($identifier) {
+    	$user = User::where('username', $identifier)->orWhere('email', $identifier)->get();
+    	return response()->json($user);
+    }
+
+}
+
+// export default function validateInput(data) {
+//   let errors = {};
+
+//   if (Validator.isEmpty(data.email)) {
+//     errors.email = 'The email field is required';
+//   }
+//   if (!Validator.isEmail(data.email)) {
+//     errors.email = 'Email is invalid';
+//   }
+
+//   if (Validator.isEmpty(data.username)) {
+//     errors.username = 'The username field is required';
+//   }
+
+//   if (Validator.isEmpty(data.password)) {
+//     errors.password = 'The password field is required';
+//   }
+
+//   if (Validator.isEmpty(data.passwordConfirmation)) {
+//     errors.passwordConfirmation = 'The password confirmation field is required';
+//   }
+
+//   if (!Validator.equals(data.password, data.passwordConfirmation)) {
+//     errors.passwordConfirmation = 'The password must match';
+//   }
+
+//   if (Validator.isEmpty(data.timezone)) {
+//     errors.timezone = 'The timezone field is required';
+//   }
+
+//   return {
+//     errors,
+//     isValid: isEmpty(errors)
+//   }
+// }
+
+// function validateInput(data, otherValidations) {
+// 	let {errors} =otherValidations(data);
+// 	return UserTypes.query({
+// 		where: {typeId: data.typeId},
+// 		orWhere: {typename: data.typename}
+// 	}).fetch().then((type) => {
+// 		if (type) {
+// 			if (type.get('typename') === data.typename) {
+// 				errors.typename = "There is type with such type-name!";
+// 			}
+// 			if (type.get('typeId') === data.typeId) {
+// 				errors.typeId = "There is type with such identifier!";
+// 			}
+// 		}
+// 		return {errors, isValid: isEmpty(errors)};
+// 	});
+// }
+
+// router.post('/', (req, res) => {
+// 	validateInput(req.body, commonValidations)
+// 		.then(({errors, isValid}) => {
+// 			if (isValid) {
+// 				const {typename, typeId} = req.body;
+// 				UserTypes.forge(
+// 					{typename, typeId},
+// 					{hasTimestamps: true}
+// 				).save().then(type => res.json({success: true}))
+// 				.catch(err => res.status(500).json({error: err}));
+// 			} else {
+// 				res.status(400).json(errors);
+// 			}
+// 		});
+// });
---
+++ routes/web.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+// Route::group([
+// 		'prefix' => '/api/users',
+// 	], function() {
+// 		Route::get('/', 'Users\UserController@store');
+// 		// Route::get('/:identifier', )	
+// 	}
+// );
+
+Route::get('/api/users/{identifier}', 'Users\UserController@sendUser');
+Route::post('/api/users', 'Users\UserController@store');
+
+
+// Route::get('issues-modal/{company}', 'CompanyController@layoutModal');
+// Route::post('issues-list/store', 'CompanyController@saveLayoutList');
+
+// Route::group([
+//     'prefix' => 'statistic',
+// ], function () {
+//     Route::get('tasks', 'StatsController@orders');
+//     Route::get('projects', 'StatsController@tasks');
+//     Route::get('managers', 'StatsController@managers');
+//     Route::get('designers', 'StatsController@designers');
+// });
+
